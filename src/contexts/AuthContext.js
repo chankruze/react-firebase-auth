ac1776d9cfe74ea32d18dd157f62d2ff5f751649
chankruze <chankruze@gmail.com>
2020-11-21 06:05:17 +0530

    Add signin, logout, reset password, update email/pass methods
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -24,6 +24,31 @@ export const AuthProvider = ({ children }) => {
     return auth.createUserWithEmailAndPassword(email, pass);
   };
 
+  // login an user with firebase
+  const signin = (email, pass) => {
+    return auth.signInWithEmailAndPassword(email, pass);
+  };
+
+  // logout an user with firebase
+  const logout = (email, pass) => {
+    return auth.signOut();
+  };
+
+  // reset password of an user with firebase
+  const resetPassword = (email) => {
+    return auth.sendPasswordResetEmail(email);
+  };
+
+  // update email of an user with firebase
+  const updateEmail = (email) => {
+    return currentUser.updateEmail(email);
+  };
+
+  // update password of an user with firebase
+  const updatePassword = (password) => {
+    return currentUser.updatePassword(password);
+  };
+
   useEffect(() => {
     // set current user
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -37,6 +62,11 @@ export const AuthProvider = ({ children }) => {
   const value = {
     currentUser,
     signup,
+    signin,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
   };
 
   return (
